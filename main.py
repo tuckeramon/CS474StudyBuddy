fd211f2ca36ffe1df98e11b4c380d8f00042ac44
tuckeramon <tja00006@mix.wvu.edu>
2025-10-21 17:30:47 -0400

    Imported Gemini Capabilities

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #This is a python File
+
+from google import genai
+
+client = genai.Client(api_key="AIzaSyC3XOsYy-Tqw25cAK_opEZ0435vZQnvKl0")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
